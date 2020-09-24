77fb85e0988a2697b62750fa56a2a3d412889dc1
Samit Koyom <samitkoyom@gmail.com>
2020-09-24 13:33:08 +0700

    update crud student

--- src/main/java/com/itgenius/springbootlayout/SpringbootlayoutApplication.java
+++ src/main/java/com/itgenius/springbootlayout/SpringbootlayoutApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootlayoutApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootlayoutApplication.class, args);
 	}
--- src/main/java/com/itgenius/springbootlayout/controller/BackendController.java
+++ src/main/java/com/itgenius/springbootlayout/controller/BackendController.java
@@ -1,5 +1,7 @@
 package com.itgenius.springbootlayout.controller;
 
+import java.util.List;
+
 import com.itgenius.springbootlayout.model.Student;
 import com.itgenius.springbootlayout.service.StudentService;
 
@@ -9,7 +11,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -27,8 +32,9 @@ public class BackendController {
 
     // List Student
     @GetMapping(value = "/backend/student")
-    public String student(){
-        return "backend/student";
+    public ModelAndView student(){
+        List<Student> list = stdService.findAll();
+        return new ModelAndView("backend/student","list",list);
     }
 
     // New Student
@@ -52,6 +58,42 @@ public class BackendController {
                 stdService.save(student);
                 return "redirect:/backend/student";
             }
+    }
+
+    // Load Edit Student Form
+    @GetMapping(value = "/backend/editstudent/{id}")
+    public String edit(@PathVariable int id,ModelMap model){
+        Student student = stdService.getOne(id);
+        model.addAttribute("student", student);
+        return "backend/editstudent";
+    }
+
+    // Edit Save Student
+    @PostMapping(value = "/backend/editsave")
+    public String editsave(@ModelAttribute("student") Student p,
+    BindingResult result){
+        if(result.hasErrors()){
+            System.out.println("has errors");
+            return "backend/editstudent";
+        }else{
+            Student std = stdService.getOne(p.getId());
+
+            std.setFirstName(p.getFirstName());
+            std.setLastName(p.getLastName());
+            std.setEmail(p.getEmail());
+
+            stdService.save(std);
+            return "redirect:/backend/student";
+        }
+    }
+
+
+    // Delet Student
+    @GetMapping(value = "/backend/deletestudent/{id}")
+    public ModelAndView delete(@PathVariable int id){
+        Student student = stdService.getOne(id);
+        stdService.delete(student);
+        return new ModelAndView("redirect:/backend/student");
     }
 
 }
